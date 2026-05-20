f5abfefe395a0ceed98033d2b33e027a5d30c62a
Masterpro32 <N00481308@upn.pe>
2026-05-19 20:24:59 -0500

    Actividad de laboratorio 4

---
+++ lab4/Lab 4.py
@@ -0,0 +1,11 @@
+# |----- MANEJO DE CADENAS Y ESTRUCTURAS EN PYTHON -----|
+
+# Declarar una cadena de texto
+texto = "Diego vive en Lima y estudia programacion"
+print("Texto original:")
+print(texto)
+
+# Obtener una subcadena
+subcadena = texto[0:6]
+print("\nSubcadena:")
+print(subcadena)
