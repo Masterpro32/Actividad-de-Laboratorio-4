de6cb14931d47afca3b7737f177b14c98dc65e19
harol1212 <harolvilchezespinoza@gmail.com>
2026-05-19 20:30:03 -0500

    Parte 5 y 6

--- lab4/Lab 4.py
+++ lab4/Lab 4.py
@@ -9,3 +9,20 @@ print(texto)
 subcadena = texto[0:6]
 print("\nSubcadena:")
 print(subcadena)
+
+# Separar el texto en partes
+palabras = texto.split()
+print("\nTexto separado en palabras:")
+print(palabras)
+
+# Implementar 2 listas paralelas
+nombres = ["Diego", "Harol", "Haziel", "Jeanpier"]
+edades = [20, 25, 22, 19]
+buscar_nombre = "Haziel"
+print("\nBusqueda en listas paralelas:")
+if buscar_nombre in nombres:
+    indice = nombres.index(buscar_nombre)
+    print("Nombre:", buscar_nombre)
+    print("Edad:", edades[indice])
+else:
+    print("Persona no encontrada")
